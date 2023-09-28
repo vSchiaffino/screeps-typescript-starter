d4a254ea51579ad78c9f804e14601d855723b2ec
Valentin Schiaffino <valenschg@gmail.com>
2023-09-28 01:04:01 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,5 @@ export const WORST_CREEP_SPAWN_COST = 200;
 
 export const KEEPERS_NEEDED = 2;
 export const UPGRADERS_NEEDED = 3;
-export const BUILDERS_NEEDED = 0;
+export const BUILDERS_NEEDED = 4;
 export const UPGRADER_TRUCK_NEEDED = 1;
